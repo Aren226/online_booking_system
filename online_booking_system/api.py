0fcd86330888aae4c79a17629ed89968d52e2a38
lingcong02 <109667639+lingcong02@users.noreply.github.com>
2024-07-11 09:51:07 +0800

    Update api.py

--- online_booking_system/api.py
+++ online_booking_system/api.py
@@ -3,14 +3,14 @@ from database import Database
 from models import Booking
 
 app = FastAPI()
-posts = []
+database = Database()
 
 @app.get("/cars")
 async def get_all_vehicle():
-    data_list = Database().get_all_vehicle()
+    data_list = database().get_all_vehicle()
     return {"Message": data_list}
 
 @app.post("/bookings", response_model=Booking)
 async def create_booking(booking: Booking):
-    insert = Database().insert_booking(booking)
+    insert = database().insert_booking(booking)
     return booking
